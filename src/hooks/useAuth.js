b76198c681b2f61421c967c6eabb404366ccaeae
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-06-28 12:36:32 +0600

    feat: add AuthContext and AuthProvider for authentication state management

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,13 @@
+import { useContext, useDebugValue } from "react";
+import { AuthContext } from "../content";
+
+export const useAuth = () => {
+  const { auth } = useContext(AuthContext);
+
+  // Use useDebugValue to provide a label for the auth state in React DevTools
+  useDebugValue(auth, (auth) =>
+    auth?.user ? "Authenticated" : "Not Authenticated"
+  );
+
+  return useContext(AuthContext);
+};
