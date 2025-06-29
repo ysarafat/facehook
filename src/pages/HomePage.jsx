f4c889d078e92b43804c0e8a696406470de96a19
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-06-29 12:30:17 +0600

    feat: implement post fetching logic with reducer; add actions for fetching posts, handle loading and error states

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useReducer } from "react";
+import { actions } from "../actions";
+import PostList from "../components/posts/PostList";
+import useAxios from "../hooks/useAxios";
+import { initialState, postReducer } from "../reucers/PostReducers";
+
 export default function HomePage() {
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    dispatch({ type: actions.post.DATE_FETCHING });
+    async function fetchPost() {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+        if (response.status === 200) {
+          dispatch({ type: actions.post.DATA_FETCHED, data: response.data });
+        }
+      } catch (error) {
+        dispatch({ type: actions.post.DATA_FETCH_ERROR, error: error.message });
+      }
+    }
+    fetchPost();
+  }, []);
+
+  if (state?.loading) {
+    return <div>Loading...</div>;
+  }
+  if (state?.error) {
+    return <div>Error: {state?.error?.message}</div>;
+  }
   return (
     <div>
-      <h1 className="text-4xl text-red-400">Welcome to Facehook</h1>
-      <p>Your social media platform for connecting with friends and family.</p>
+      <PostList posts={state?.posts} />
     </div>
   );
 }
---
+++ src/reucers/PostReducers.js
@@ -0,0 +1,37 @@
+import { actions } from "../actions";
+
+const initialState = {
+  posts: [],
+  loading: false,
+  error: null,
+};
+
+const postReducer = (state, action) => {
+  switch (action.type) {
+    case actions.post.DATE_FETCHING: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case actions.post.DATA_FETCHED: {
+      return {
+        ...state,
+        loading: false,
+        posts: action.data,
+      };
+    }
+    case actions.post.DATA_FETCH_ERROR: {
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export { initialState, postReducer };
